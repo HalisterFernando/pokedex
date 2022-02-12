ba66e9d053913ee7c87a66e53e29f52eb32f6331
Seu nome <halisterfernando@hotmail.com>
2022-02-12 14:57:43 -0300

    criado pokecard

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ const pokeInfo = document.getElementById('poke-info');
 const fetchPokémon = async () => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName.value.toLowerCase()}`)
     const data = await response.json();
-       
+    console.log(data)
+    const pokeImg = data.sprites.versions['generation-iv']['diamond-pearl'].front_default;
+    const flavor = await fetch(data.species.url);
+    const flavorData = await flavor.json();
+    console.log(flavorData)
+    
+   const img = document.createElement('img')
+   img.src = pokeImg
+
+   pokeInfo.appendChild(img)
+    
 };
 
 searchBtn.addEventListener('click', fetchPokémon)
